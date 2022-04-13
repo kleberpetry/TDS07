bb881b8859155fb575904b83e791b276d402ed03
Kleber Petry <kleberpetry@gmail.com>
2022-04-13 19:45:14 -0300

    Update controle de estoque.c

--- controle de estoque.c
+++ controle de estoque.c
@@ -1,10 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<locale.h>
+struct produto{
+	int codigo;
+	char nome[50];
+	double valorUnitario;
+	int estoque;
+};
+struct produto produtos[200];
+
+cadastrarProduto(){
+	
+}
+
 
 main(){
 	setlocale(LC_ALL, "Portuguese");
+	int x;
+	system("cls");
+	printf("\nDigite 1 para cadastrar um produto\nDigite 2 para dar entrada em estoque\nDigite 3 para ajustar um estoque\nDigite 4 para listar todos os produtos\nDigite 5 para buscar um produto por codigo\nDigite sua opção: ");
 	
+	//cadastro produto
+	//entrada de estoque
+	//ajuste de estoque
+	//listar todos produtos
+	//busca produto
+	//venda
 	
 	
 	
